95677e9844113de6c8d89105c72c733948e208ed
laurenmcgrathwork <laurenmcgrath.work@gmail.com>
2017-02-25 19:54:20 -0500

    weatherApp up to menu layer

---
+++ src/c/splash_window.c
@@ -0,0 +1,50 @@
+#include <pebble.h>
+#include "splash_window.h"
+
+Window *splashWindow;
+GBitmap *splashImage;
+BitmapLayer *splashImageLayer;
+TextLayer *splashImageTitle;
+
+void splash_window_load(Window *window){
+  //where we put layer 'magic'
+  //set up image
+    //create a gbit map from resource of kitten
+      //pass that gbit map on gbit layer
+  Layer *window_layer = window_get_root_layer(window);  
+  
+  splashImage = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_SPLASH);
+  //set up text layer
+  splashImageLayer = bitmap_layer_create(GRect(0,0,144,120)); //x coord,y coord, width, height
+  
+  bitmap_layer_set_bitmap(splashImageLayer, splashImage);
+  //text_layer_set_text(splashImageTitle, "Weather App");
+  //text_layer_set_text_alignment(splashImageTitle GTextAlignmentCenter);
+  bitmap_layer_set_compositing_mode(splashImageLayer, GCompOpSet);
+  layer_add_child(window_layer, bitmap_layer_get_layer(splashImageLayer)); //add something ontop of deepest layer
+
+  splashImageTitle = text_layer_create(GRect(0,120, 144, 50)); //always draw from top to bottom
+  text_layer_set_text(splashImageTitle, "Weather App");
+  text_layer_set_text_alignment(splashImageTitle, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(splashImageTitle));
+}
+void splash_window_unload(Window *window){
+  //when taken off stack, distroy resources
+  gbitmap_destroy(splashImage);
+  bitmap_layer_destroy(splashImageLayer);
+  text_layer_destroy(splashImageTitle);
+}
+void splash_window_create(){
+  splashWindow = window_create();
+  window_set_window_handlers(splashWindow, (WindowHandlers){//when pushed onto display
+    .load = splash_window_load,
+    .unload = splash_window_unload
+  });
+}
+void splash_window_destroy(){
+  window_destroy(splashWindow);
+}
+Window *splash_window_get_window(){
+  //convience function that returns spalsh window to us
+  return splashWindow;
+}
---
+++ src/pkjs/app.js
@@ -0,0 +1,4 @@
+Pebble.addEventListener("ready", function(e){//when you load app, function is called when phone is ready with interaction
+  console.log("ready for interaction");    
+});
+
