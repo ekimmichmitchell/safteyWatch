1d21f594f80931c674c09925f05cca5c685af521
laurenmcgrathwork <laurenmcgrath.work@gmail.com>
2017-02-26 03:29:39 -0500

    log location coordinates to console

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,3 +1,24 @@
+function success(pos) {
+  console.log('lat= ' + pos.coords.latitude + ' lon= ' + pos.coords.longitude);
+}
+
+function error(err) {
+  console.log('location error (' + err.code + '): ' + err.message);
+}
+
+/* ... */
+
+// Choose options about the data returned
+var options = {
+  enableHighAccuracy: true,
+  maximumAge: 10000,
+  timeout: 10000
+};
+
+// Request current position
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+
 Pebble.addEventListener("appmessage", function(e){//when you load app, function is called when phone is ready with interaction
   console.log("Got testkey with value "+ e.payload.testkey);  //event.messageFromPayLoad(withdata inside)
 });
